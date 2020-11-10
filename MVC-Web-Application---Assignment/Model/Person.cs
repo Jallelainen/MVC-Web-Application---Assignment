29010783a5aeae44246a81c96b188e3437b721da
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-11-10 15:02:31 +0100

    added person + project classes. finished requirements for assignment 1

---
+++ MVC-Web-Application---Assignment/Model/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Web_Application___Assignment.Model
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private string street;
+        private int postnum;
+        private string city;
+        private int telnum;
+        private string email;
+
+        public string Name { get { return name; } }
+        public int Age { get { return age; } }
+        public string Street { get { return street; } }
+        public int PostNum { get { return postnum; } }
+        public string City { get { return city; } }
+        public string Email { get { return email; } }
+        public int TelNum { get { return telnum; } }
+
+        public Person (string personName, int personAge, string personStreet, int personPostnum, string personCity, int personTel, string personEmail)
+        {
+            name = personName;
+            age = personAge;
+            street = personStreet;
+            postnum = personPostnum;
+            city = personCity;
+            telnum = personTel;
+            email = personEmail;
+        }
+
+
+    }
+}
---
+++ MVC-Web-Application---Assignment/Model/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Web_Application___Assignment.Model
+{
+    public class Project
+    {
+
+        private string name;
+        private string description;
+        private string link;
+
+        public string Name { get { return name; } }
+        public string Description { get { return description; } }
+        public string Link { get { return link; } }
+
+        public Project (string projectName, string projectDescription, string projectLink)
+        {
+            name = projectName;
+            description = projectDescription;
+            link = projectLink;
+        }
+
+    }
+}
